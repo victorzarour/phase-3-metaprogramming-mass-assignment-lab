5af0d07a40c2e31777d9481074e8cdf709e49d87
Victor <vx_zz@hotmail.com>
2022-06-30 08:53:41 -0400

    commit tree 9420d063ea34664df189d8002143f071ea033572

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,13 @@
+require 'pry'
+
 class Person
-  # your code here
+
+  def initialize(attributes)
+    attributes.each do |key, value|
+      binding.pry
+      self.class.attr_accessor(key)
+      self.send("#{key}=", value)
+    end
+  end
+
 end
